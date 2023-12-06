3084537db1b0e504051298c3d1554b97eb79e5de
Joaquim <devjoaquim@gmail.com>
2023-12-06 08:36:11 +0100

    First idea - Day 6

---
+++ day6/src/main.rs
@@ -0,0 +1,60 @@
+use std::{env,fs,process};
+
+// Idea: each input has T and R (time and record) and we want to multiply every number in the range
+// of the integers solving the inequality T*x - x^2 >= R, i.e x^2 - T*x + R <= 0, so from
+// ceil(- sqrt( (T/2)^2 - R ) - T/2) until floor(sqrt( (T/2)^2 - R ) - T/2)
+// CORRECTION: We want to multiply by x_i * (T - x_i)
+
+fn run1(input: &str) -> u32 {
+    0
+}
+
+fn run2(input: &str) -> u32 {
+    0
+}
+
+fn main() {
+    let mut args = env::args();
+    let filepath;
+    args.next();
+    if let Some(s) = args.next() {
+        filepath = s;
+    }
+    else {
+        eprintln!("Give me a file name! I must feeds on files! Aaargh!");
+        process::exit(1);
+    }
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    let res = run1(&input);
+    println!("{res}");
+}
+
+#[test]
+fn example1() {
+    let input = fs::read_to_string("test.txt").unwrap();
+    let res = run1(&input);
+    assert_eq!(res,42);
+}
+
+//#[test]
+//fn input1() {
+    //let input = fs::read_to_string("input.txt").unwrap();
+    //let res = run1(&input);
+    //assert_eq!(res,42);
+//}
+
+//#[test]
+//fn example2() {
+    //let input = fs::read_to_string("test.txt").unwrap();
+    //let res = run2(&input);
+    //assert_eq!(res,42);
+//}
+
+//#[test]
+//fn input2() {
+    //let input = fs::read_to_string("input.txt").unwrap();
+    //let res = run2(&input);
+    //assert_eq!(res,42);
+//}
