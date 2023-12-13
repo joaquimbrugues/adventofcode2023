00c1ea36db39b0da4287d282c867408afe167e0e
Joaquim <devjoaquim@gmail.com>
2023-12-13 21:20:33 +0100

    Day 13, Part 1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,7 +1,54 @@
 use std::{env,fs,process};
 
 fn run1(input: &str) -> u32 {
-    0
+    let mut res = 0;
+    for block in input.split("\n\n") {
+        let layout: Vec<Vec<bool>> = block.lines().map(|line| {
+            line.chars().map(|c| {
+                match c {
+                    '#' => true,
+                    '.' => false,
+                    _ => unreachable!(),
+                }
+            }).collect()
+        }).collect();
+
+        for s in 1..layout.len() {
+            let mut i = 0;
+            let mut mirrors = true;
+            while mirrors && s + i < layout.len() && i < s {
+                let mut j = 0;
+                while mirrors && j < layout[s].len() {
+                    mirrors &= layout[s-i-1][j] == layout[s+i][j];
+                    j += 1;
+                }
+                i += 1;
+            }
+            if mirrors {
+                res += 100 * s as u32;
+                break;
+            }
+        }
+
+        for s in 1..layout[0].len() {
+            let mut j = 0;
+            let mut mirrors = true;
+            while mirrors && s + j < layout[0].len() && j < s {
+                let mut i = 0;
+                while mirrors && i < layout.len() {
+                    mirrors &= layout[i][s-j-1] == layout[i][s+j];
+                    i += 1;
+                }
+                j += 1;
+            }
+            if mirrors {
+                res += s as u32;
+                break
+            }
+        }
+    }
+
+    res
 }
 
 fn run2(input: &str) -> u32 {
