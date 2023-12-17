7dd74cba83c59ad87ae3cdc64ab01233760d652f
Joaquim <devjoaquim@gmail.com>
2023-12-17 13:12:44 +0100

    Day 17, part 1

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -14,25 +14,19 @@ impl Orientation {
         }
     }
 
-    fn advance(&self, pt: (isize, isize)) -> [(isize, isize); 6] {
+    fn next_pos(&self, pt: (isize, isize)) -> (isize, isize) {
         use Orientation::*;
         match self {
-            Vert => [
-                (pt.0 - 3, pt.1),
-                (pt.0 - 2, pt.1),
-                (pt.0 - 1, pt.1),
-                (pt.0 + 1, pt.1),
-                (pt.0 + 2, pt.1),
-                (pt.0 + 3, pt.1),
-            ],
-            Hori => [
-                (pt.0, pt.1 - 3),
-                (pt.0, pt.1 - 2),
-                (pt.0, pt.1 - 1),
-                (pt.0, pt.1 + 1),
-                (pt.0, pt.1 + 2),
-                (pt.0, pt.1 + 3),
-            ],
+            Vert => (pt.0 + 1, pt.1),
+            Hori => (pt.0, pt.1 + 1),
+        }
+    }
+
+    fn next_neg(&self, pt: (isize, isize)) -> (isize, isize) {
+        use Orientation::*;
+        match self {
+            Vert => (pt.0 - 1, pt.1),
+            Hori => (pt.0, pt.1 - 1),
         }
     }
 }
@@ -84,17 +78,37 @@ fn run1(input: &str) -> i32 {
     let mut visited = HashSet::new();
     heap.push(Node::from((0,0,0,Orientation::Hori)));
     heap.push(Node::from((0,0,0,Orientation::Vert)));
-    visited.insert((0,0,Orientation::Hori));
-    visited.insert((0,0,Orientation::Vert));
 
     while let Some(node) = heap.pop() {
-        println!("{node:?}");
-        if node.x == height - 1 && node.y == width - 1 {
+        if node.x == height && node.y == width {
             return - node.loss;
         }
 
-        let mut acc_loss = 0;
-        //TODO: Add elements to the heap, accumulate loss
+        if !visited.contains(&(node.x, node.y, node.ori)) {
+            visited.insert((node.x, node.y, node.ori));
+            let mut acc_loss = node.loss;
+            let mut next = (node.x, node.y);
+            for _ in 0..3 {
+                next = node.ori.next_pos(next);
+                if let Some(loss) = map.get(&next) {
+                    acc_loss += loss;
+                    heap.push(Node::from((next.0, next.1, acc_loss, node.ori.opp())))
+                } else {
+                    break;
+                }
+            }
+            acc_loss = node.loss;
+            let mut next = (node.x, node.y);
+            for _ in 0..3 {
+                next = node.ori.next_neg(next);
+                if let Some(loss) = map.get(&next) {
+                    acc_loss += loss;
+                    heap.push(Node::from((next.0, next.1, acc_loss, node.ori.opp())))
+                } else {
+                    break;
+                }
+            }
+        }
     }
     0
 }
@@ -128,12 +142,12 @@ fn example1() {
     assert_eq!(res, 102);
 }
 
-//#[test]
-//fn input1() {
-    //let input = fs::read_to_string("input.txt").unwrap();
-    //let res = run1(&input);
-    //assert_eq!(res,42);
-//}
+#[test]
+fn input1() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let res = run1(&input);
+    assert_eq!(res, 742);
+}
 
 //#[test]
 //fn example2() {
